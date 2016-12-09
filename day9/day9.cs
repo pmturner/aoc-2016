d0715b2f62476a0750fe26a046bdf0218e13502f
Paul Turner <pmturner211@gmail.com>
2016-12-09 15:06:38 -0600

    day9 init

---
+++ day9/day9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aoc2016
+{
+    public class day9
+    {
+        public static void Main(string[] args)
+        {
+            string data = System.IO.File.ReadAllText(@"C:\home\projects\aoc-2016\day9\input.txt");
+            string decompressedData = "";
+
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (data[i] != "(")
+                {
+                    decompressedData += data[i];
+                }
+                else
+                {
+                    
+                }
+            }
+            Console.WriteLine(data);
+        }
+    }
+}
