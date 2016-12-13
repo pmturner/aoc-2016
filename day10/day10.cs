7e969337d24a82131bf66300aff9a808f1a1bc72
Paul Turner <Paul Turner@mind-over-data.local>
2016-12-13 16:10:08 -0600

    folder renaming and day10 init

---
+++ day10/day10.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace aoc2016
+{
+    public class day10
+    {
+        
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
