d4e02f222b66c7397e90c8fa4a42944be024c634
Paul Turner <mrturnerphysics@gmail.com>
2016-12-15 21:15:49 -0600

    day15 init

--- day10/day10.cs
+++ day10/day10.cs
@@ -4,11 +4,25 @@ namespace aoc2016
 {
     public class day10
     {
-        
-
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\home\projects\aoc-2016\day10\input.txt");
+            int highValueTarget = 61;
+            int lowValueTarget = 17;
+
+            foreach(string line in lines)
+            {
+                string[] lineItem = line.Split(' ');
+                
+                if (lineItem[0] == "value")
+                {
+                    GiveValueToBot(lineItem[1], lineItem[5]);
+                }
+                else
+                {
+                    BotProcess(lineItem);;
+                }
+            }
         }
     }
 }
---
+++ day15/day15.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aoc2016
+{
+    public class day15
+    {
+        public static void Main(string[] args)
+        {
+            string[] discs = System.IO.File.ReadAllLines(@"C:\home\projects\aoc-2016\day15\input.txt");
+            List<int> positions = new List<int>();
+            List<int> initialPositions = new List<int>();
+
+            foreach (string disc in discs)
+            {
+                string[] discBreak = disc.Split(' ');
+
+                positions.Add(Convert.ToInt32(discBreak[3].TrimEnd('.')));
+                initialPositions.Add(Convert.ToInt32(discBreak[11].TrimEnd('.')));
+            }
+
+            var timer = 0;
+
+            for (var i = 0; i < discs.Length; i++) {
+                
+            }
+        }
+    }
+}
